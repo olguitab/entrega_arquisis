4624b385d3398e66091afb9dedbe5f2ab3980a0b
Rodrigo Meza Aranda <rmezaa@uc.cl>
2024-09-30 02:53:26 -0300

    Saved wallet controller

--- e0-olguitab-master/src/wallet/wallet.controller.ts
+++ e0-olguitab-master/src/wallet/wallet.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Post, Body, HttpStatus, Get, Param, Query, NotFoundException, BadRequestException } from '@nestjs/common';
 import { WalletService } from './wallet.service';
+import { Types } from 'mongoose';
 
 @Controller('wallet')
 export class WalletController {
@@ -9,5 +10,10 @@ export class WalletController {
   async addMoney(@Body() requestBody: any): Promise<void> {
     const { user_id, amount } = requestBody;
     return this.walletService.addMoneyToWallet(user_id, amount);
+  }
+
+  @Get('balance/:user_id')
+  async getBalance(@Param('user_id') user_id: string): Promise<number> {
+    return this.walletService.getWalletBalance(user_id );
   }
 }
