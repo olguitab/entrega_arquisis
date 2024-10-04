669e1419de39e9216309f40a7feef5d53b20aab8
ralevi02 <raimundo.lecaros@uc.cl>
2024-10-04 00:05:18 -0300

    FINAL FINAL

--- e0-olguitab-master/mqtt-service/mqtt-service.js
+++ e0-olguitab-master/mqtt-service/mqtt-service.js
@@ -91,7 +91,7 @@ client.on('message', async (topic, message) => {
     }
   }
 
-  /*else if (topic === 'fixtures/history') {
+  else if (topic === 'fixtures/history') {
     try {
       const parsedMessage = JSON.parse(JSON.parse(message.toString()));
       console.log('Received message on fixtures/history, sending to app...');
@@ -103,7 +103,7 @@ client.on('message', async (topic, message) => {
     } catch (error) {
       console.error('Error processing MQTT message: HISTORIA', error);
     }
-  }*/
+  }
 });
 
 async function fetchAndPublish() {
