9d05cb0dad6bfe9818c42fe45f39029d8d479d71
ralevi02 <raimundo.lecaros@uc.cl>
2024-10-28 19:14:06 -0300

    cd test 5

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo "Stop Aplication"
-docker compose --file /home/ubuntu/docker-compose.production.yml down
+echo "Stop Application"
+
+# Log the current working directory
+echo "Current directory is: $(pwd)"
+
+# Log the contents of the current directory
+echo "Listing contents:"
+ls -la
+
+# Stop the application using docker-compose
+docker compose --file docker-compose.production.yml down
